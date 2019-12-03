6217b992cd2f7ca7897ef13c4c4f9a9a7914bf12
Max Escher <max@maxescher.com>
2019-12-02 21:24:18 -0500

    Add pdf reader, and extract text from pdf.

--- applecard_pdf_to_csv.py
+++ applecard_pdf_to_csv.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 # Imports
+import PyPDF2
 
 # Import AppleCard pdf statement
+applecard_pdf = open(input("Location of pdf: "), 'rb')
+ac_obj = PyPDF2.PdfFileReader(applecard_pdf)
+ac_total_pages = ac_obj.numPages
+
+print("Number of pages: {pages}".format(pages=ac_total_pages))
 
 # Convert pdf statement to readable text
+
+for page in range(ac_total_pages):
+  print("PAGE NUMBER: {page_num}".format(page_num=page))
+  page_text = ac_obj.getPage(page).extractText()
+  print(page_text)
+  print("\n")
+
+applecard_pdf.close()
 
 # Grab relevant data from text
 
