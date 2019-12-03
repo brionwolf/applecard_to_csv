774169bc7aae7b3745cc45f2a809136a17ffd0e2
Max Escher <max@maxescher.com>
2019-12-02 19:12:36 -0500

    add pseudo code

---
+++ applecard_pdf_to_csv.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Imports
+
+# Import AppleCard pdf statement
+
+# Convert pdf statement to readable text
+
+# Grab relevant data from text
+
+# Translate text information for csv format
+
+# export as csv
